00a3a603a2f93bb6d6d1af9cead75328d2d3ee84
Ravi K R <raviram.mysore@gmail.com>
2023-07-18 11:33:08 +0530

    4 pets added to a collection

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,28 +1,47 @@
-﻿// Create the first pet
+﻿
+// Create the pets
 Pet pet1 = new Pet();
 pet1.Id = 1;
 pet1.Name = "Fluffy";
 pet1.City = "San Francisco";
 pet1.IsVaccinated = true;
 
-// Display the first pet
-DisplayPetInfo(pet1);
-
-// Create the second pet
 Pet pet2 = new Pet();
 pet2.Id = 2;
 pet2.Name = "Spot";
 pet2.City = "New York";
 pet2.IsVaccinated = false;
 
-// Display the second pet
-DisplayPetInfo(pet2);
+Pet pet3 = new Pet();
+pet3.Id = 3;
+pet3.Name = "Bella";
+pet3.City = "Los Angeles";
+pet3.IsVaccinated = true;
 
-void DisplayPetInfo(Pet pet1)
+Pet pet4 = new Pet();
+pet4.Id = 4;
+pet4.Name = "Max";
+pet4.City = "Chicago";
+pet4.IsVaccinated = false;
+
+// Create a list and add the pets to it
+List<Pet> pets = new List<Pet>();
+pets.Add(pet1);
+pets.Add(pet2);
+pets.Add(pet3);
+pets.Add(pet4);
+
+// Display the pets
+foreach (Pet pet in pets)
 {
-    Console.WriteLine($"Id: {pet1.Id}");
-    Console.WriteLine($"Name: {pet1.Name}");
-    Console.WriteLine($"City: {pet1.City}");
-    Console.WriteLine($"Is Vaccinated: {pet1.IsVaccinated}");
+    Display(pet);
+}
+
+void Display(Pet pet)
+{
+    Console.WriteLine($"Id: {pet.Id}");
+    Console.WriteLine($"Name: {pet.Name}");
+    Console.WriteLine($"City: {pet.City}");
+    Console.WriteLine($"Is Vaccinated: {pet.IsVaccinated}");
     Console.WriteLine();
 }
