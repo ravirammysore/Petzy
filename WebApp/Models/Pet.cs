c69bc81d105729deecc054fdd29fb923100f8c3a
Ravi K R <raviram.mysore@gmail.com>
2023-07-20 15:45:09 +0530

    Display pet list

---
+++ WebApp/Models/Pet.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApp.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public bool IsVaccinated { get; set; }
+        public int AgeInMonths { get; set; }
+        public PetType PetType { get; set; }
+    }
+
+    public enum PetType
+    {
+        Dog,
+        Cat
+    }
+}
--- WebApp/Pages/Pets/Index.cshtml.cs
+++ WebApp/Pages/Pets/Index.cshtml.cs
@@ -1,16 +1,28 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.Models;
+using System.Collections.Generic;
 
 namespace WebApp.Pages.Pets
 {
     public class IndexModel : PageModel
     {
-        public string FirstPetName { get; set; }
-        public string SecondPetName { get; set; }
+        public List<Pet> Pets { get; set; }
+
         public void OnGet()
         {
-            FirstPetName = "Fluffy";
-            SecondPetName = "Bella";
+            Pets = new List<Pet>
+            {
+                new Pet { Id = 1, Name = "Fluffy", City = "San Francisco",
+                    IsVaccinated = true, AgeInMonths = 4, PetType = PetType.Cat },
+
+                new Pet { Id = 2, Name = "Bella", City = "San Francisco",
+                    IsVaccinated = true, AgeInMonths = 6, PetType = PetType.Dog },
+                
+                new Pet { Id = 3, Name = "Spot", City = "Chicago",
+                    IsVaccinated = false, AgeInMonths = 12,  PetType = PetType.Cat }
+                
+                // Add more pets as needed...
+            };
         }
     }
 }
