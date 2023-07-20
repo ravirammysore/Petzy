de75ece858c45623091757c933179783bdd2b40e
Ravi K R <raviram.mysore@gmail.com>
2023-07-20 15:13:38 +0530

    Display pet names

--- WebApp/Pages/Pets/Index.cshtml.cs
+++ WebApp/Pages/Pets/Index.cshtml.cs
@@ -5,8 +5,12 @@ namespace WebApp.Pages.Pets
 {
     public class IndexModel : PageModel
     {
+        public string FirstPetName { get; set; }
+        public string SecondPetName { get; set; }
         public void OnGet()
         {
+            FirstPetName = "Fluffy";
+            SecondPetName = "Bella";
         }
     }
 }
