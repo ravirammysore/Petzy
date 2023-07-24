a1f8c4d7fced49eb54ad4e4b5b37b12d2bde332e
Ravi K R <raviram.mysore@gmail.com>
2023-07-24 20:57:56 +0530

    Add remaining fields

--- WebApp/Pages/Pets/Create.cshtml.cs
+++ WebApp/Pages/Pets/Create.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Diagnostics;
 using WebApp.Models;
 
@@ -8,14 +9,31 @@ namespace WebApp.Pages.Pets
     public class CreateModel : PageModel
     {
         [BindProperty]
-        public Pet Pet { get; set; }       
+        public Pet Pet { get; set; }
 
+        public List<SelectListItem> DropDownItems { get; set; }
+
+        public IActionResult OnGet()
+        {
+            var petTypeValues = Enum.GetValues(typeof(PetType)).Cast<PetType>();
+
+            DropDownItems = petTypeValues.Select(pt => new SelectListItem
+            {
+                Value = ((int)pt).ToString(),
+                Text = pt.ToString()
+            }).ToList();
+
+            return Page();
+        }
         public void OnPost()
         {
             //save it to our pet list later
             Debug.WriteLine(Pet.Name);
             Debug.WriteLine(Pet.City);
             Debug.WriteLine(Pet.AgeInMonths);
+            
+            Debug.WriteLine(Pet.IsVaccinated);
+            Debug.WriteLine(Pet.PetType);
         }
     }
 }
