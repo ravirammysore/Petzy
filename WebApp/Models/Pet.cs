3c88d098ca6bd7166f51e0c89fbe32626a16312e
Ravi K R <raviram.mysore@gmail.com>
2023-07-31 16:08:21 +0530

    Annotations added to the class

--- WebApp/Models/Pet.cs
+++ WebApp/Models/Pet.cs
@@ -1,14 +1,32 @@
-﻿namespace WebApp.Models
-{
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{   
+
     public class Pet
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(15, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 15 characters.")]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(15, MinimumLength = 2, ErrorMessage = "City must be between 2 and 15 characters.")]
         public string City { get; set; }
+
+        [Display(Name = "Is Vaccinated")]
         public bool IsVaccinated { get; set; }
+
+        [Required]
+        [Range(1, 100, ErrorMessage = "Age must be between 1 and 100 months.")]
+        [Display(Name = "Age (Months)")]
         public int AgeInMonths { get; set; }
+
+        [Display(Name = "Type of Pet")]
         public PetType PetType { get; set; }
     }
+
 
     public enum PetType
     {
