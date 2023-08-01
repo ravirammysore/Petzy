a66c0e0775c070ab4326636c599538a6ab1768ae
Ravi K R <raviram.mysore@gmail.com>
2023-08-01 15:39:10 +0530

    show pets index page using EF

---
+++ WebApp/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Pet> Pets { get; set; }
+    }
+}
--- WebApp/Pages/Pets/Index.cshtml.cs
+++ WebApp/Pages/Pets/Index.cshtml.cs
@@ -2,16 +2,26 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebApp.Models;
 using System.Collections.Generic;
 using WebApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApp.Pages.Pets
 {
     public class IndexModel : PageModel
     {
         public List<Pet> Pets { get; set; }
-
         public void OnGet()
         {
-            Pets = InMemoryDatabase.Pets;           
+            string server = "(localdb)\\mssqllocaldb";
+            string database = "PetzyDB";
+            string trustedConnection = "True";
+
+            string connectionString = $"Server={server};Database={database};Trusted_Connection={trustedConnection};";
+
+            var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+            var context = new DataContext(optionsBuilder.Options);
+            
+            Pets = context.Pets.ToList();
         }
     }
 }
