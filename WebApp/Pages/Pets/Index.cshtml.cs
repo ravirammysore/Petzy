a8e6c78f36f2ac52935545a8108dc0c80118d3c1
Ravi K R <raviram.mysore@gmail.com>
2023-08-05 14:12:56 +0530

    Use dependency injection

--- WebApp/Pages/Pets/Index.cshtml.cs
+++ WebApp/Pages/Pets/Index.cshtml.cs
@@ -7,21 +7,16 @@ using Microsoft.EntityFrameworkCore;
 namespace WebApp.Pages.Pets
 {
     public class IndexModel : PageModel
-    {
+    {        
+        private readonly DataContext _context;
+        public IndexModel(DataContext context)
+        {
+            _context = context;
+        }
         public List<Pet> Pets { get; set; }
         public void OnGet()
-        {
-            string server = "(localdb)\\mssqllocaldb";
-            string database = "PetzyDB";
-            string trustedConnection = "True";
-
-            string connectionString = $"Server={server};Database={database};Trusted_Connection={trustedConnection};";
-
-            var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
-            optionsBuilder.UseSqlServer(connectionString);
-            var context = new DataContext(optionsBuilder.Options);
-            
-            Pets = context.Pets.ToList();
+        {           
+            Pets = _context.Pets.ToList();
         }
     }
 }
--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,5 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddRazorPages();
+
+var conString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(conString));
 
 var app = builder.Build();
 
