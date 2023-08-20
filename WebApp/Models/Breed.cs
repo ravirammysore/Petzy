d0473af916bdf67add9c92f8c6bd371d816fa4bc
Ravi K R <raviram.mysore@gmail.com>
2023-08-20 14:47:32 +0530

    display name of certain properties of Breed

--- WebApp/Models/Breed.cs
+++ WebApp/Models/Breed.cs
@@ -1,11 +1,15 @@
-﻿namespace WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
 {
     public class Breed
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; } 
+        public string Description { get; set; }
+        [Display(Name = "Average Weight (Kilos)")]
         public double AverageWeight { get; set; }
+        [Display(Name = "Average Lifespan (Years)")]
         public int AverageLifeSpan { get; set; }
         public ICollection<Pet> Pets { get; set; }
     }
