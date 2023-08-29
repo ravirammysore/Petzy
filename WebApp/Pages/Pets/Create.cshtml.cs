25b1d9b797d96e9cb0dacf4013bfb188f80d9eaa
Ravi K R <raviram.mysore@gmail.com>
2023-08-29 15:29:36 +0530

    show breed dropdown in create page

--- WebApp/Pages/Pets/Create.cshtml.cs
+++ WebApp/Pages/Pets/Create.cshtml.cs
@@ -8,30 +8,58 @@ using WebApp.Models;
 namespace WebApp.Pages.Pets
 {
     public class CreateModel : PageModel
-    {       
+    {
         private readonly DataContext _context;
         public CreateModel(DataContext context)
         {
             _context = context;
         }
-        public List<SelectListItem> DropDownItems { get; set; }
+        public List<SelectListItem> GenderDropDownItems { get; set; }
+        public List<SelectListItem> BreedDropDownItems { get; set; }
 
         public IActionResult OnGet()
-        {            
-            PopulateDropdown();
-
+        {
+            PopulateGenderDropdown();
+            PopulateBreedDropdown();
             return Page();
         }
 
-        private void PopulateDropdown()
+        private void PopulateGenderDropdown()
         {
             // Create a new list with hardcoded SelectListItem objects
-            DropDownItems = new List<SelectListItem>
+            GenderDropDownItems = new List<SelectListItem>
             {
                 new SelectListItem { Value = "0", Text = "Male" },
                 new SelectListItem { Value = "1", Text = "Female" }
             };
         }
+        private void PopulateBreedDropdown()
+        {
+            BreedDropDownItems = new List<SelectListItem>();
+
+            var defaultOption = new SelectListItem
+            {
+                Value = "",
+                Text = "--Select a Breed--",
+                Selected = true,
+                Disabled = true
+            };
+
+            BreedDropDownItems.Add(defaultOption);
+
+            var breeds = _context.Breeds.OrderBy(b => b.Name).ToList();
+
+            foreach (var breed in breeds)
+            {
+                var item = new SelectListItem
+                {
+                    Value = breed.Id.ToString(),
+                    Text = breed.Name
+                };
+                BreedDropDownItems.Add(item);
+            }
+        }
+
 
         [BindProperty]
         public Pet Pet { get; set; }
@@ -40,8 +68,9 @@ namespace WebApp.Pages.Pets
             ModelState.Remove("Pet.Breed");
 
             if (!ModelState.IsValid)
-            {                
-                PopulateDropdown();
+            {
+                PopulateGenderDropdown();
+                PopulateBreedDropdown();
                 return Page();
             }
 
